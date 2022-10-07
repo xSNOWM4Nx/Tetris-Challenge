a93597d648da3ab05b7ebbdd93b5a7b7d288e7ad
Neuweiler Daniel <daniel.neuweiler@bruderer.com>
2022-10-07 10:07:13 +0200

    Program start updated

---
+++ src/TetrisChallenge.UI.Web/ProgramConfiguration.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace TetrisChallenge.UI.Web
+{
+    public class ProgramConfiguration
+    {
+        #region fields
+
+        public const string StartupLoggerName = "B3-Bootstrapper";
+
+        private readonly WebApplicationBuilder _builder;
+
+        #endregion
+        #region ctor
+
+        public ProgramConfiguration(WebApplicationBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        #endregion
+
+        public void Initialize()
+        {
+            var app = _builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ReactClientApp";
+
+                if (app.Environment.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+
+            app.Run();
+        }
+    }
+}
---
+++ src/TetrisChallenge.UI.Web/ProgramServices.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TetrisChallenge.UI.Web
+{
+    public class ProgramServices
+    {
+        #region fields
+
+        private readonly WebApplicationBuilder _builder;
+
+        #endregion
+        #region ctor
+
+        public ProgramServices(WebApplicationBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        #endregion
+
+        public void Initialize()
+        {
+            IServiceCollection services = _builder.Services;
+
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ReactClientApp/build";
+            });
+        }
+    }
+}
